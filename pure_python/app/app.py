686214399a2228c308858d278de17fcb8715ec68
Jose Ariza <l.jlac001@gmail.com>
2024-05-23 21:03:01 -0500

    Adds base pokemons search app

--- pure_python/app/app.py
+++ pure_python/app/app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    pokemons = []
+    return render_template("search.html", **{"pokemons": pokemons})
 
 
 if __name__ == "__main__":
